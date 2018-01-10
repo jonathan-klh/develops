f5b989929de49d232bc539f94bdd45d371711688
Axel Prouin <axelprouin@gmail.com>
2018-01-10 11:14:39 +0100

    entity skill

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -8,6 +8,9 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\User;
+use AppBundle\Entity\UserSkill;
+use AppBundle\Form\UserSkillType;
 use AppBundle\Form\UserType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
@@ -34,19 +37,34 @@ class UserController extends Controller
      */
     public function modifsInfosAction(Request $request) {
         $user = $this->getUser();
+        $userSkill = new UserSkill();
+
         $form = $this->createForm(UserType::class, $user);
+        $formSkill = $this->createForm(UserSkillType::class, $userSkill);
 
         $form->handleRequest($request);
+        $formSkill->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()){
             $em = $this->getDoctrine()->getManager();
             $em->persist($user);
             $em->flush();
+
             return $this->redirectToRoute('fos_user_profile_show');
+        }
+
+        if ($formSkill->isSubmitted() && $formSkill->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $userSkill->setUser($user);
+            $em->persist($userSkill);
+            $em->flush();
+
+            return $this->redirectToRoute('informations');
         }
 
         return $this->render('@FOSUser/More/edit_infos.html.twig', array(
             'form' => $form->createView(),
+            'formSkill' => $formSkill->createView(),
         ));
     }
 }
--- src/AppBundle/Entity/UserSkill.php
+++ src/AppBundle/Entity/UserSkill.php
@@ -3,12 +3,20 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+use Doctrine\ORM\Mapping\UniqueConstraint;
 
 /**
  * UserSkill
  *
- * @ORM\Table(name="user_skill")
+ * @ORM\Table(name="user_skill",
+ *    uniqueConstraints={
+ *        @UniqueConstraint(name="user_skill_unique",
+ *            columns={"user_id", "skill_id"})
+ *    }
+ * )
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserSkillRepository")
+ *
  */
 class UserSkill
 {
@@ -22,12 +30,12 @@ class UserSkill
     private $id;
 
     /**
-     * @ORM\ManyToMany(targetEntity="User", mappedBy="username")
+     * @ORM\ManyToOne(targetEntity="User")
      */
     private $user;
 
     /**
-     * @ORM\ManyToMany(targetEntity="Skill", mappedBy="name")
+     * @ORM\ManyToOne(targetEntity="Skill")
      */
     private $skill;
 
@@ -72,5 +80,52 @@ class UserSkill
     {
         return $this->level;
     }
-}
 
+    /**
+     * Set user
+     *
+     * @param \AppBundle\Entity\User $user
+     *
+     * @return UserSkill
+     */
+    public function setUser(\AppBundle\Entity\User $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \AppBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set skill
+     *
+     * @param \AppBundle\Entity\Skill $skill
+     *
+     * @return UserSkill
+     */
+    public function setSkill(\AppBundle\Entity\Skill $skill = null)
+    {
+        $this->skill = $skill;
+
+        return $this;
+    }
+
+    /**
+     * Get skill
+     *
+     * @return \AppBundle\Entity\Skill
+     */
+    public function getSkill()
+    {
+        return $this->skill;
+    }
+}
